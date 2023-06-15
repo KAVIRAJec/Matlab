8860961aa0a9ee03c92400971cde453aaca8505d
Kaviraj A <kaviraj.ec21@bitsathy.ac.in>
2023-06-15 18:14:49 +0530

    Add files via upload

---
+++ Image Processing/day10.m
@@ -0,0 +1,18 @@
+clc;
+clear all;
+close all;
+x=imread('C:\Users\Student\Downloads\download.jpeg');
+imshow(x);
+figure;
+se=strel('disk',35);
+bg=imopen(x,se);
+x2=x-bg;
+imshow(x2);
+figure;
+x4=rgb2gray(x2);
+x3= (x4);
+imshow(x3);
+bw=imbinarize(x3);
+bw=bwareaopen(bw,50);
+cc=bwconncomp(bw,4);
+cc.NumObjects;
